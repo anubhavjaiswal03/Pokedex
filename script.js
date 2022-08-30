f2b8d6dbb9144d6a0ebf5c47b6e7ae123182de35
Anubhav Jaiswal <36488000+anubhavjaiswal03@users.noreply.github.com>
2022-08-30 19:37:58 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
+const poke_container = document.getElementById('poke-container');
+const pokemon_count = 150;
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#be98d7',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5',
+};
 
+const fetchPokemons = async () => {
+	for (let i = 1; i <= pokemon_count; i++) {
+		await getPokemon(i);
+	}
+};
+
+const getPokemon = async (id) => {
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+	const res = await fetch(url);
+	const data = await res.json();
+	createPokemonCard(data);
+};
+
+function createPokemonCard(pokemon) {
+	console.log(pokemon);
+	const pokemonEl = document.createElement('div');
+	pokemonEl.classList.add('pokemon');
+
+	const pokemanInnerHtml = `
+  <div class="img-container">
+  <img
+    src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.id
+			.toString()
+			.padStart(3, '0')}.png"
+    alt=""
+  />
+  </div>
+  <div class="info">
+    <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
+    <h3 class="name">${
+			pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+		}</h3 >
+    <small class="type">Type: <span>${
+			pokemon.types[0].type['name']
+		}</span></small>
+  </div>
+  `;
+
+	pokemonEl.innerHTML = pokemanInnerHtml;
+	pokemonEl.style.backgroundColor = colors[pokemon.types[0].type['name']];
+	poke_container.appendChild(pokemonEl);
+}
+
+fetchPokemons();
